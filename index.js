2a10c155486a8232e902dd2c87f43f4d8c421e97
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2022-03-22 19:48:35 -0300

    refatorando código

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ const PORT = precess.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (_request, response) => {
-  try {
-    response.status(200).json({ message: 'Tudo certo' });
-  } catch (error) {
-    console.log(error.message);
-    return response.status(500).json({ message: 'Algo deu errado' });
-  }
+  response.send('Vamo que vamo Trybe!');
 });
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));

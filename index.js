49e574317b53639af3097415dd057a818b6cb5b2
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2022-03-22 20:06:44 -0300

    Usando o exemplo do Pedro(Professor)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const app = express();
-const PORT = precess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/', (_request, response) => {
-  response.send('Vamo que vamo Trybe!');
+  return response.status(501).end();
 });
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));

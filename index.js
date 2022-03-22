899fd500e36ed6df492dec952785bea79d240542
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2022-03-22 20:35:26 -0300

    new: adicionando variavel de ambiente

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || null;
+const SERVER_ENV = process.env.SERVER_ENV || null;
 
 app.get('/', (_request, response) => {
+  if (SERVER_ENV) {
+    return response.status(200).send({ message: `Rodando no ambiente ${SERVER_ENV}` });
+  }
   return response.status(200).send({ message: 'Dummy endpoint' });
 });
 

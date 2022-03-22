eaf5dffd3090302f807641fb769ae28e069258be
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2022-03-22 20:10:41 -0300

    Ainda não sei o que da dando errado

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
+const PORT = process.env.PORT || null;
 
 app.get('/', (_request, response) => {
   return response.status(501).end();

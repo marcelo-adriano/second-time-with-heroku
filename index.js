3e8a0a0f5a715289c052d554cdb316f50540fc33
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2022-03-22 20:14:27 -0300

    new: mensagem para quem acessar a página

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || null;
 
 app.get('/', (_request, response) => {
-  return response.status(501).end();
+  return response.status(200).send({ message: 'Dummy endpoint' });
 });
 
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));

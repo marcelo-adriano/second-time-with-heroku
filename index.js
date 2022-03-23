fc71a1a58234a01f0d78f97acda3a4acd7f04bf3
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2022-03-22 21:57:30 -0300

    fix: Trocando mensagem da aplicação

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const SERVER_ENV = process.env.SERVER_ENV || null;
 
 app.get('/', (_request, response) => {
   if (SERVER_ENV) {
+    if (SERVER_ENV === 'test') {
+      return response.status(200).send({ message: `Rodando no ambiente ${SERVER_ENV} com CICD/Github actions` });
+    }
     return response.status(200).send({ message: `Rodando no ambiente ${SERVER_ENV}` });
   }
   return response.status(200).send({ message: 'Dummy endpoint' });
